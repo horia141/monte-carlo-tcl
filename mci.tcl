d8684c05d697cce909367c7a23f56747988d1017
Horia Coman <horia141@gmail.com>
2010-11-09 01:09:05 +0200

    Small refactorings and two check functions.
    * I've done some refactoring and small bug fixing (crashes on
      bad input).
    
    * I've added the "checkCommandLine" and "postCheckFnData" functions
      which implement a lot of input safety checks. I've only got
      "checkFnData" to do and initial error checking should be done.
      Any other error checks, I'll add as bug-fixes later on.

--- mci.tcl
+++ mci.tcl
@@ -1,24 +1,80 @@
 #!/usr/bin/env tclsh
 
+set InfoMessage {
+Usage: mci.tcl -f [FILE] -N [SAMPLECOUNT] -step [MINMAXSTEP] [...PARAMETERS]
+
+Computes the definite integral of multi-dimensional functions using Monte
+Carlo Integration. The target function, it's domain and external parameters
+are defined in an .mci file specified as a path argument to the -f option.
+To control the precision of the computation, use the -N option (with a number
+of iterations as an argument: higher values correspond to more precise results,
+but cause higher runtimes). An important part in the integration process is
+finding out the minimum and maximum of the defined functions. These are hard
+to compute by hand, so the application must determine them as well. The number
+passed as an argument to the -step option determines the granularity with which
+the input space is scanned for extrema points. Generally, values between
+0 and 1 are good, with smaller values better from a precision point of view.
+The same precision-runtime tradeoff encounterd for the -N option occurs here
+as well. Finally, parameters defined in a function's "Params" section are
+passed into the application as arguments to appropriately named command line
+options. The option format employed is -[parameter name] [parameter value].
+
+A parameter cannot be named "f","N","step","help" or "info". These are
+reserved names and, when present as command line options, will change the
+behaviour of the whole program. Valid characters are just the alphanumerics
+and "_" in usual C identifier rules (a parameter name cannot start with a
+digit). Also, the parameter's value must pass all the type and range
+constraints in the definition file.
+}
+
+set HelpMessage {
+Usage: mci.tcl -f [FILE] -N [SAMPLECOUNT] -step [MINMAXSTEP] [...PARAMETERS]
+       mci.tcl -help : This help message.
+       mci.tcl -info : A tutorial message.
+
+-f               Path to function definitions file.
+-N               Number of iterations used in integration.
+-step            Input space granularity for MinMax detection.
+-[name] [value]  A parameter from the "Params" section of a function. This
+                 sets the parameter [name] to the value [value]. Used when
+                 integrating and in MinMax. See "mci.tcl -info" for more.
+}
+
 proc main {argv} {
-    if {![checkCommandLine $argv]} {
-	error "A"
+    set clErrors [checkCommandLine $argv]
+
+    if {[llength $clErrors] != 0} {
+	printErrors $clErrors
+	exit 1
     }
 
     array set clData [parseClData $argv]
 
-    set fnHandle [open $clData(FnFile) r]
-    set fnText [read $fnHandle]
-    close $fnHandle
+    set readResult [catch {
+	set fnHandle [open $clData(FnFile) r]
+	set fnText [read $fnHandle]
+	close $fnHandle
+    } readErrors]
 
-    if {![checkFnData $fnText]} {
-	error "A"
+    if {$readResult != 0} {
+	puts $readErrors
+	exit 1
+    }
+
+    set fnErrors [checkFnData $fnText]
+
+    if {[llength $fnErrors] != 0} {
+	printErrors $fnErrors
+	exit 1
     }
 
     array set fnData [parseFnData $fnText]
 
-    if {![postCheckFnData [array get fnData]]} {
-	error "A"
+    set postCheckErrors [postCheckFnData [array get clData] [array get fnData]]
+
+    if {[llength $postCheckErrors] != 0} {
+	printErrors $postCheckErrors
+	exit 1
     }
 
     foreach fnName $fnData(FnNames) {
@@ -71,11 +127,11 @@ proc _recMinMax {positionName fnName arClData arFnData begs ends index} {
 	array set rangesEnd {}
 	
 	foreach {paramName} $fnData(Fns,$fnName,ParamsNames) {
-	    set function [regsub -all $paramName $function "\$__$paramName"]
+	    set function [regsub -all :$paramName $function "\$__$paramName"]
 	}
 
 	foreach {rangeName} $fnData(Fns,$fnName,RangesNames) {
-	    set function [regsub -all $rangeName $function "\$__$rangeName"]
+	    set function [regsub -all :$rangeName $function "\$__$rangeName"]
 	}
 
 	foreach {rangeName} $fnData(Fns,$fnName,RangesNames) {
@@ -86,6 +142,10 @@ proc _recMinMax {positionName fnName arClData arFnData begs ends index} {
 
     while {[lindex $position $index] <= [lindex $ends $index]} {
 	if {$index == ([llength $position] - 1)} {
+	    foreach {paramName} $fnData(Fns,$fnName,ParamsNames) {
+		set __$paramName $clData(Params,$paramName)
+	    }
+
 	    for {set i 0} {$i < [llength $position]} {incr i} {
 		set __[lindex $fnData(Fns,$fnName,RangesNames) $i] [lindex $position $i]
 	    }
@@ -129,11 +189,11 @@ proc integrateFunction {fnName arClData arFnData} {
     set area [expr 1.0 * ($max - $min)]
 
     foreach {paramName} $fnData(Fns,$fnName,ParamsNames) {
-	set function [regsub -all $paramName $function "\$__$paramName"]
+	set function [regsub -all :$paramName $function "\$__$paramName"]
     }
 
     foreach {rangeName} $fnData(Fns,$fnName,RangesNames) {
-	set function [regsub -all $rangeName $function "\$__$rangeName"]
+	set function [regsub -all :$rangeName $function "\$__$rangeName"]
     }
 
     foreach {rangeName} $fnData(Fns,$fnName,RangesNames) {
@@ -143,9 +203,11 @@ proc integrateFunction {fnName arClData arFnData} {
 	set area [expr $area * ($rangesEnd($rangeName) - $rangesBeg($rangeName))]
     }
 
-    puts $function
-
     for {set i 0} {$i < $clData(SampleSetSize)} {incr i} {
+	foreach {paramName} $fnData(Fns,$fnName,ParamsNames) {
+	    set __$paramName $clData(Params,$paramName)
+	}
+
     	foreach {rangeName} $fnData(Fns,$fnName,RangesNames) {
     	    set __$rangeName [randInRange $rangesBeg($rangeName) $rangesEnd($rangeName)]
     	}
@@ -166,24 +228,120 @@ proc integrateFunction {fnName arClData arFnData} {
 }
 
 proc checkCommandLine {argv} {
-    return 1
+    global InfoMessage
+    global HelpMessage
+
+    set clErrors {}
+
+    if {[lsearch $argv {-info}] != -1} {
+	lappend clErrors $InfoMessage
+    } elseif {[lsearch $argv {-help}] != -1} {
+	lappend clErrors $HelpMessage
+    } else {
+	if {[llength $argv] % 2 != 0} {
+	    lappend clErrors "Invalid number of options for command line : [join $argv]"
+	}
+
+	foreach {optName optValue} $argv {
+	    if {![string equal [string index $optName 0] {-}]} {
+		lappend clErrors "Invalid format for option name : $optName"
+	    }
+
+	    if {![regexp {[a-zA-Z_][a-zA-Z0-9_]*} $optName]} {
+		lappend clErrors "Invalid format for option name : $optName"
+	    }
+	}
+    }
+
+    return $clErrors
 }
 
 
 proc checkFnData {fnText} {
-    return 1
+    return {}
 }
 
-proc postCheckFnData {arFnData} {
-    return 1
+proc postCheckFnData {arClData arFnData} {
+    array set clData $arClData
+    array set fnData $arFnData
+
+    set postCheckErrors {}
+
+    set allParams {}
+    set clParams {}
+
+    foreach {fnName} $fnData(FnNames) {
+	set allParams [concat $allParams $fnData(Fns,$fnName,ParamsNames)]
+    }
+
+    set allParams [lsort -unique $allParams]
+    set clParams [lsort -unique $clData(ParamsNames)]
+
+    # Check all required parameters appear in command line.
+
+    foreach {fnName} $fnData(FnNames) {
+	foreach {paramName} $fnData(Fns,$fnName,ParamsNames) {
+	    if {[lsearch -sorted $clParams $paramName] == -1} {
+		lappend postCheckErrors "Parameter \"$paramName\" required by function \"$fnName\" is not specified!"
+	    }
+	}
+    }
+
+    # Check all command line supplied parameters are required by a function.
+
+    foreach {paramName} $clParams {
+	if {[lsearch -sorted $allParams $paramName] == -1} {
+	    lappend postCheckErrors "Option \"-$paramName $clData(Params,$paramName)\" specified an unknown parameter!"
+	}
+    }
+
+    # Check supplied values are of type real and between the specified limits.
+
+    foreach {fnName} $fnData(FnNames) {
+	foreach {paramName} $fnData(Fns,$fnName,ParamsNames) {
+	    if {[lsearch -sorted $clParams $paramName] != -1} {
+		switch -- $fnData(Fns,$fnName,Params,$paramName,Type) {
+		    Real {
+			if {![string is double -strict $clData(Params,$paramName)]} {
+			    lappend postCheckErrors "Parameter \"$paramName\" for function \"$fnName\" does not look like a real number!"
+			} elseif {$clData(Params,$paramName) < $fnData(Fns,$fnName,Params,$paramName,Beg)} {
+			    lappend postCheckErrors "Parameter \"$paramName\" for function \"$fnName\" is smaller than the allowed minimum of \"$fnData(Fns,$fnName,Params,$paramName,Beg)\"!"
+			} elseif {$clData(Params,$paramName) > $fnData(Fns,$fnName,Params,$paramName,End)} {
+			    lappend postCheckErrors "Parameter \"$paramName\" for function \"$fnName\" is greater than the allowed maximum of \"$fnData(Fns,$fnName,Params,$paramName,End)\"!"
+			}
+		    }
+
+		    default {
+			error "Unsupported execution path! Should not have arrived here"
+		    }
+		}
+	    }
+	}
+    }
+
+    return $postCheckErrors
 }
 
 proc parseClData {argv} {
     array set clData {}
-
+    
     set clData(FnFile) fn.mci
     set clData(MinMaxStep) 0.1
-    set clData(SampleSetSize) 200000
+    set clData(SampleSetSize) 10000
+    set clData(ParamsNames) [list]
+
+    foreach {optName optValue} $argv {
+	switch -- [string range $optName 1 end] {
+	    f {set clData(FnFile) $optValue}
+	    N {set clData(SampleSetSize) $optValue}
+	    step {set clData(MinMaxStep) $optValue}
+	    default {
+		set paramName [string range $optName 1 end]
+		lappend clData(ParamsNames) $paramName
+		set clData(Params,$paramName) $optValue
+	    }
+	}
+    }
 
     return [array get clData]
 }
@@ -223,7 +381,7 @@ proc parseFnData {fnText} {
 	# Extract function parameter information.
 
 	foreach {paramName paramBody} $paramsBody {
-	    switch [lindex $paramBody 0] {
+	    switch -- [lindex $paramBody 0] {
 		Real {
 		    lappend fnData(Fns,$fnName,ParamsNames) $paramName
 
@@ -241,7 +399,7 @@ proc parseFnData {fnText} {
 	# Extract function range information.
 
 	foreach {rangeName rangeBody} $rangesBody {
-	    switch [lindex $rangeBody 0] {
+	    switch -- [lindex $rangeBody 0] {
 		Real {
 		    lappend fnData(Fns,$fnName,RangesNames) $rangeName
 
@@ -295,6 +453,12 @@ proc printFnData {arFnData} {
 
 proc randInRange {beg end} {
     return [expr rand() * ($end - $beg) + $beg]
+}
+
+proc printErrors {errorsList} {
+    foreach {errorMessage} $errorsList {
+	puts $errorMessage
+    }
 }
 
 main $argv
